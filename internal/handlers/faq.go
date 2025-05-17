b331d0730f6361befb70578242d9ab8452bf77a5
BstevensG4G <yabill_2000@yahoo.com>
2025-05-16 19:15:41 -1000

    Fixed capitalization on header and spelling in faq.tmpl

--- internal/handlers/faq.go
+++ internal/handlers/faq.go
@@ -14,11 +14,11 @@ func FAQHandler(c echo.Context) error {
 	questions := []FAQItems{
 		{"What type of power outlets do you use? Do I need a special adapter?"},
 		{"How late are your bars open, and when can I buy alcohol?"},
-		{"At what age are you allowed to dring?"},
+		{"At what age are you allowed to drink?"},
 		{"What languages are spoken on the Island?"},
 		{"What happens if I get sick, where would I go?"},
 		{"How concerned should we be about our personal safety and security?"},
-		{"What days are restruants and attractions open or closed?"},
+		{"What days are restaurants and attractions open or closed?"},
 		{"What currency is accepted at your businesss establishments?"},
 	}
 	return c.Render(http.StatusOK, "base", map[string]any{
